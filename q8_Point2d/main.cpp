ceb46760b49d44a6ed2073846f8065a3f151408a
Gavin Gavington <GavinGavington@gavins-macbook-air.local>
2018-10-19 15:26:14 -0700

    Start

--- q8_Point2d/main.cpp
+++ q8_Point2d/main.cpp
@@ -6,9 +6,31 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class Point2d
+{
+private:
+    double m_x;
+    double m_y;
+public:
+    Point2d() : m_x { 0.0 }, m_y { 0.0 }
+    {
+        
+    }
+    Point2d(double x, double y) : m_x { x }, m_y { y }
+    {
+        
+    }
+    void print()
+    {
+        std::cout << "Point2d(" << m_x << ", " << m_y << ");" << std::endl;
+    }
+};
+int main()
+{
+    Point2d first;
+    Point2d second(3.0, 4.0);
+    first.print();
+    second.print();
+    
     return 0;
 }

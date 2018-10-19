769ba6c9e49715488c87c98934e646a6f91fd469
Gavin Gavington <GavinGavington@gavins-macbook-air.local>
2018-10-19 15:37:56 -0700

    Add test

--- q8_Point2d/main.cpp
+++ q8_Point2d/main.cpp
@@ -27,6 +27,7 @@ int main()
     Point2d second(3.0, 4.0);
     first.print();
     second.print();
+    std::cout << "Distance between two points: " << first.distanceTo(second) << '\n';
     
     return 0;
 }

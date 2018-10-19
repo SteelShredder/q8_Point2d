d2380faa48895f14d83e571ba8b518bc377c3b22
Gavin Gavington <GavinGavington@gavins-macbook-air.local>
2018-10-19 15:05:28 -0700

    Initial Commit

---
+++ q8_Point2d/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  q8_Point2d
+//
+//  Created by Gavin Gavington on 10/19/18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
